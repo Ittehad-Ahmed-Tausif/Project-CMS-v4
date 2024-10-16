8a0d36fa0ace87e3650f8922237a1cdaf13c2566
Tausif <ittehad.ahmed.tausif@g.bracu.ac.bd>
2024-10-16 21:29:52 +0600

    changes made

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
-# Define the scope of the application
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+gc = gspread.service_account(filename="E:\Programming\credentials.json")
 
-# Add credentials to the account
-creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/credentials.json', scope)
+sh = gc.open("Project CMS Main Server")
 
-# Authorize the clientsheet 
-client = gspread.authorize(creds)
 
-# Get the instance of the Spreadsheet
-spreadsheet = client.open('Name of your Google Sheet')
-
-# Get the first sheet of the Spreadsheet
-sheet = spreadsheet.sheet1
-
-# Read data from the sheet
-data = sheet.get_all_records()
-print(data)
-
-# Write data to the sheet
-sheet.update_cell(2, 2, 'Updated Value')  # Update cell B2 with 'Updated Value'
+second_sheet =sh.worksheet("test")
+print(second_sheet.get("A1"))
+# print(sh.sheet1.get('G4'))
